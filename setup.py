1c2ddeb604ed4bc352d774a6ac3a3586d5c0c5a7
Axel Haustant <noirbizarre@gmail.com>
2017-08-17 20:19:23 +0200

    Update or pin dependencies to their latest version

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'click>=4.0',
-        'colorama>=0.3.3',
-        'Fiona==1.7.5',
-        'msgpack-python==0.4.7',
-        'pymongo>=3.0',
-        'Shapely==1.5.17',
-        'Flask',
-        'requests',
+        'click==6.7',
+        'colorama==0.3.9',
+        'Fiona==1.7.8',
+        'msgpack-python==0.4.8',
+        'pymongo==3.5.0',
+        'Shapely==1.6b4',
+        'Flask==0.12.2',
+        'requests==2.18.4',
     ],
     extras_require={
-        'i18n': ['transifex-client==0.11.1b0', 'Babel>=1.3']
+        'i18n': ['transifex-client==0.12.4', 'Babel==2.4.0']
     },
     entry_points='''
         [console_scripts]
