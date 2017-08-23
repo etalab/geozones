45f977b25890124e4047b6f36fba7c4b0ee8a6be
Axel Haustant <noirbizarre@gmail.com>
2017-08-23 18:09:50 +0200

    Logos improvements

--- geozones/logos.py
+++ geozones/logos.py
@@ -4,9 +4,9 @@ import tarfile
 import click
 import requests
 
-from .tools import info, success
+from .tools import info, success, unicodify
 
-DBPEDIA_MEDIA_URL = 'http://commons.wikimedia.org/wiki/Special:FilePath/'
+DBPEDIA_MEDIA_URL = 'https://commons.wikimedia.org/wiki/Special:FilePath/'
 LOGOS_FOLDER_PATH = 'logos'
 LOGOS_FILENAME = 'geologos.tar.xz'
 
@@ -26,6 +26,8 @@ def fetch_logos(zones, dl_dir):
     """
     info('Fetching logos from Wikimedia')
     path = os.path.join(dl_dir, LOGOS_FOLDER_PATH)
+    logfile_path = os.path.join(dl_dir, 'logos.log')
+    logfile = open(logfile_path, 'w')
     if not os.path.exists(path):
         os.makedirs(path)
     downloaded = 0
@@ -43,15 +45,20 @@ def fetch_logos(zones, dl_dir):
                 continue
             filepath = os.path.join(path, filename)
             if os.path.exists(filepath):
+                logfile.write('{0} : skipped\n'.format(filename))
+                logfile.flush()
                 continue
-            url = DBPEDIA_MEDIA_URL + filename
+            url = DBPEDIA_MEDIA_URL + unicodify(filename)
             r = requests.get(url, stream=True, headers=HEADERS)
-            if r.status_code == 404:
+            if r.status_code != 200:
+                logfile.write('{0} : {1}\n'.format(url, r.status_code))
+                logfile.flush()
                 continue
             with open(filepath, 'wb') as file_destination:
                 for chunk in r.iter_content(chunk_size=1024):
                     file_destination.write(chunk)
                 downloaded += 1
+    logfile.close()
     success('{0} logos fetched for {1} candidates', downloaded, count)
 
 
@@ -61,8 +68,5 @@ def compress_logos(dl_dir, dist_dir):
     path = os.path.join(dl_dir, LOGOS_FOLDER_PATH)
     info('Compressing logos to {filename}', filename=filename)
     with tarfile.open(filename, 'w:xz') as txz:
-        for (dirpath, dirnames, filenames) in os.walk(path):
-            for name in filenames:
-                txz.add(os.path.join(path, name))
-            break
+        txz.add(path, 'logos')
     success('Compressing done')
