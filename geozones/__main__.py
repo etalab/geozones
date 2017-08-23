397cbe5b94d1171d121892a912608470177158de
Axel Haustant <noirbizarre@gmail.com>
2017-08-23 18:41:00 +0200

    Fix translations packaging

--- geozones/__main__.py
+++ geozones/__main__.py
@@ -283,7 +283,7 @@ def dist(ctx, pretty, split, compress, serialization, keys):
         filename = 'geozones-translations.tar.xz'
         with ok('Compressing to {0}'.format(filename)):
             with tarfile.open(filename, 'w:xz') as txz:
-                txz.add(join(ctx.obj['home'], 'translations'), 'translations')
+                txz.add(join(ctx.obj['home'], 'geozones', 'translations'), 'translations')
 
         filename = 'geozones-split.tar.xz' if split else 'geozones.tar.xz'
 
@@ -294,7 +294,7 @@ def dist(ctx, pretty, split, compress, serialization, keys):
                 for name in filenames:
                     txz.add(name)
                 # Add translations
-                txz.add(join(ctx.obj['home'], 'translations'), 'translations')
+                txz.add(join(ctx.obj['home'], 'geozones', 'translations'), 'translations')
 
     os.chdir(ctx.obj['home'])
 
